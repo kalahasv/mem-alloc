b0acd221f8e01fe914459a42d568427cdd11689c
Vikasni Kalahasthi <kalahasv@uci.edu>
2022-11-03 00:21:00 -0700

    added helper method given in notes, commented out

--- main.c
+++ main.c
@@ -29,6 +29,15 @@ int findFirstFree(){
     p = p + (*p & -2);    \\ goto next block (word addressed) 
     */  
 }
+/*
+void addblock(ptr p, int len) { //note: int* p is listed as "ptr p" in the notes
+    int newsize = ((len + 1) >> 1) << 1;  // round up to even
+    int oldsize = *p & -2;                // mask out low bit
+    *p = newsize | 1;                     // set new length
+    if (newsize < oldsize)
+    *(p+newsize) = oldsize - newsize;   // set length in remaining part of block
+}      
+*/                                 
 
 void eval(char **argv, int argc, enum ALGORITHM algo){
 
